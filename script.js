850a75d012c9f575fc4809ff84a6b46e9ae15480
lavisse <chloe.lavisse80@gmail.com>
2018-10-24 12:13:00 +0200

    envoi des fichiers

---
+++ script.js
@@ -0,0 +1,8 @@
+// on déclare un module testApp qu'on affecte à notre application app
+var testApp = angular.module('app',[]);
+// on rattache notre controller testCtrl à notre module testApp auquel
+// on injecte une dépendance $scope
+testApp.controller('testCtrl', function($scope){
+  $scope.firstSentence = 'L\'Axolotl, Ambystoma mexicanum, est une espèce d\'urodèles de la famille des Ambystomatidae.';
+  $scope.secondSentence = 'L\'ORNITHORYNQUE (ORNITHORHYNCHUS ANATINUS) EST UN ANIMAL SEMI-AQUATIQUE ENDÉMIQUE DE L\'EST DE L\'AUSTRALIE, Y COMPRIS LA TASMANIE.';
+});
